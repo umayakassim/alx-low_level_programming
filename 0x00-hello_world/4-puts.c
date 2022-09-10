05884f92fcda39605161b591dbab3cffc1e623df
umayakassim <-->
2022-09-09 23:39:00 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,11 @@
 * main - print the string in the put function
 *
 * Description : using the main function
-* this program prints " Programming is like building a multingual puzzle
-* Return: 0
+* this program prints "Programming is like building a multilingual puzzle
+* Retrun: 0
 */
- int main (void)
- {
-   puts("\"Programming is like building a multilingual puzzle
-");
-return (0);
+int main(void)
+{
+     puts("\"Programming is like bulding a multilingual puzzle");
+     return (0);
 }
