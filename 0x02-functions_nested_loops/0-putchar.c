b5b6d4924d69201bd86bb832076f4d6c6941164b
umayakassim <-->
2022-09-13 22:03:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
-#include "holherton.h"
+#include "holberton.h"
 
 /**
-*_putchar - writes the character c to stdout
-*@c: The character to print
-*
-*Return:On success 1.
-*On error,-1 is returned,and error is set appropriate.
-*/
-int_putchar(char c)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-   return (write(1, 8c, 1));
+	return (write(1, &c, 1));
 }
