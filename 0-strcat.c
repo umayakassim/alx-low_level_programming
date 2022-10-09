cd6a55908f9236dafede76556c63cfe4c6886a02
umayakassim <-->
2022-10-08 21:42:57 -0700

    a function that concatenates two strings.

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,50 +1,44 @@
-#include "main.h"
-/**
+nclude "main.h"                                                                                                                    
 
- * _strcat - concatenates two strings,
+                                                                                                                                     
 
- * @dest: destination.
+/**                                                                                                                                  
+ *
+ *  * strcat - Concatenates the string pointed to by @src, including the x                                                     
+ *
+ *   *          null byte, to the end of the string pointed to by @dest.                                                                 
+ *
+ *    * @dest: A pointer to the string to be concatenated upon.                                                                           
+ *
+ *     * @src: The source string to be appended to @dest.                                                                                  
+ *
+ *      *                                                                                                                                   
+ *
+ *       * Return: A pointer to the destination string @dest.                                                                                
+ *
+ *        */                                                                                                                                  
 
- * @src: source.
+char *_strcat(char *dest, const char *src)                                                                                            
 
- * Return: the pointer to dest.
+{                                                                                                                                    
 
- */
+	        int index = 0, dest_len = 0;                                                                                                 
 
-char *_strcat(char *dest, char *src)
+		                                                                                                                                     
 
-{
+		        while (dest[index++])                                                                                                        
 
-	int count = 0, count2 = 0;
+				                dest_len++;                                                                                                          
 
+			                                                                                                                                     
 
+			        for (index = 0; src[index]; index++)                                                                                         
 
-	while (*(dest + count) != '\0')
+					                dest[dest_len++] = src[index];                                                                                       
 
-	{
+				                                                                                                                                     
 
-		count++;
-
-	}
-
-
-
-	while (count2 >= 0)
-
-	{
-
-		*(dest + count) = *(src + count2);
-
-		if (*(src + count2) == '\0')
-
-			break;
-
-		count++;
-
-		count2++;
-
-	}
-
-	return (dest);
+				        return (dest);                                                                                                               
 
 }
+
