1e5adbefa2e8b51f54c9d00764e2861eb3b70df2
umayakassim <-->
2022-10-11 22:56:59 -0700

    hot fix

--- 1-strncat.c
+++ 1-strncat.c
@@ -1,15 +1,28 @@
 #include "main.h"
 #include <string.h>
-
 /**
-* _strncat -> function to append some charx
-* @dest: first param
-* @str: second param
-* @n: 3rd param
-* Return: string
-*/
+ * _strncat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * @n: amount of bytes used from src.
+ * Return: the pointer to dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-  strncat(dest, src, n);
-  return (dest);
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
