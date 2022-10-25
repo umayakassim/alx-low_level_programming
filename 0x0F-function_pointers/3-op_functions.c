531137e2bd067efd59df5b34ebca1c62ba1d06f4
Ukassim-dev <69954868+umayakassim@users.noreply.github.com>
2022-10-25 18:09:25 +0300

    create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Operation of addition.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Addition of ops.
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - Operation of substraction.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Substraction of ops.
+ */
+
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - Operation of multiplication.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Multiplication of ops.
+ */
+
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - Operation of division.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Division of ops.
+ */
+
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - Operation of module.
+ * @a: Operator 1
+ * @b: Operator 2
+ * Return: Module of ops.
+ */
+
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
