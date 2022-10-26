cdbeb59d188889f91f3dfe2888dd3cac8cef8b1b
Ukassim-dev <69954868+umayakassim@users.noreply.github.com>
2022-10-26 09:23:17 +0300

    initial commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,24 @@
+/*
+ * File: 1-init_dog.c
+ * Author: Umaya Kassim
+ */
+
 #include "dog.h"
+#include <stdlib.h>
 
 /**
-* init_dog - initializes variable of type dog
-* @d: pointer to variable
-* @name: pointer to name
-* @age: age of dog
-* @owner: pointer to owner of dog
-*
-* Return: no return
-*/
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: The dog to be initialized.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
